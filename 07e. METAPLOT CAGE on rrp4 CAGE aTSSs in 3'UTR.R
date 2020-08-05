d3e231de21366718d971622ed08144cd5fa9197e
Axel Thieffry <axel.thieffry@gmail.com>
2020-08-05 17:55:33 +0200

    Update 07e. METAPLOT CAGE on rrp4 CAGE aTSSs in 3'UTR.R

--- 07e. METAPLOT CAGE on rrp4 CAGE aTSSs in 3'UTR.R
+++ 07e. METAPLOT CAGE on rrp4 CAGE aTSSs in 3'UTR.R
@@ -13,14 +13,14 @@ library(robustbase)
 'rename' <- dplyr::rename
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
-setwd('~/Dropbox/Axel_Arabidopsis_Flagellin/ANALYSIS_TSSstory/07 - metagene plots antisense')
+setwd('~/masked_path/07 - metagene plots antisense')
 
 # CAGE SCALED METAPLOT ON RNASEQ RRP4 ADEGS (FROM DEEPTOOLS) ####
 # ---------------------------------------------------------------
-pp <- read.delim('~/Dropbox/Axel_Arabidopsis_Flagellin/ANALYSIS_TSSstory/07 - metagene plots antisense/matrix_CAGE_plus_BED_plus.mat', skip=2)[, -1]
-mm <- read.delim('~/Dropbox/Axel_Arabidopsis_Flagellin/ANALYSIS_TSSstory/07 - metagene plots antisense/matrix_CAGE_minus_BED_minus.mat', skip=2)[, -1]
-pm <- read.delim('~/Dropbox/Axel_Arabidopsis_Flagellin/ANALYSIS_TSSstory/07 - metagene plots antisense/matrix_CAGE_plus_BED_minus.mat', skip=2)[, -1]
-mp <- read.delim('~/Dropbox/Axel_Arabidopsis_Flagellin/ANALYSIS_TSSstory/07 - metagene plots antisense/matrix_CAGE_minus_BED_plus.mat', skip=2)[, -1]
+pp <- read.delim('~/masked_path/matrix_CAGE_plus_BED_plus.mat', skip=2)[, -1]
+mm <- read.delim('~/masked_path/matrix_CAGE_minus_BED_minus.mat', skip=2)[, -1]
+pm <- read.delim('~/masked_path/matrix_CAGE_plus_BED_minus.mat', skip=2)[, -1]
+mp <- read.delim('~/masked_path/matrix_CAGE_minus_BED_plus.mat', skip=2)[, -1]
 
 (samples <- colnames(pp) %>% str_remove('\\..*') %>% unique())
 
@@ -38,18 +38,12 @@ names(matrix_anti) <- samples
 # compute average
 mat_sense_ave <- sapply(matrix_sense, function(x) colMeans(as.matrix(x))) %>% as_tibble()
 mat_anti_ave <- sapply(matrix_anti, function(x) colMeans(as.matrix(x))) %>% as_tibble()
-# compute median
-# mat_sense_med <- sapply(matrix_sense, function(x) colMedians(as.matrix(x))) %>% as_tibble()
-# mat_anti_med <- sapply(matrix_anti, function(x) colMedians(as.matrix(x))) %>% as_tibble()
 
 # add position
 mat_sense_ave %<>% mutate('position'=1:240, 'direction'='sense')
 mat_anti_ave %<>% mutate('position'=1:240, 'direction'='anti')
-#mat_sense_med %<>% mutate('position'=1:240, 'direction'='sense')
-#mat_anti_med %<>% mutate('position'=1:240, 'direction'='anti')
 
 # plot average
-
 gg_ave_sense <- rbind(mat_sense_ave, mat_anti_ave) %>%
   melt(id.vars=c('position', 'direction'), variable.name='genotype', value.name='averageRPM') %>%
   subset(direction=='sense') %>%
@@ -73,14 +67,3 @@ gg_ave_anti <- rbind(mat_sense_ave, mat_anti_ave) %>%
          scale_color_brewer(palette='Set2', name='', direction=-1)
 
 gg_ave_sense + gg_ave_anti + plot_layout(ncol=1)
-
-# gg_med <- rbind(mat_sense_med, mat_anti_med) %>%
-#   melt(id.vars=c('position', 'direction'), variable.name='genotype', value.name='averageRPM') %>%
-#   subset(direction=='anti') %>%
-#   ggplot(aes(x=position, y=ifelse(direction=='anti', -averageRPM, averageRPM), group=interaction(direction, genotype), col=genotype)) +
-#          geom_vline(xintercept=c(20, 220), lty=2) +
-#          geom_line(lwd=.65) +
-#          cowplot::theme_cowplot() + theme(aspect.ratio=1, legend.position='bottom', legend.direction=2) +
-#          labs(title='CAGE at rrp4 CAGE 3UTR aTSSs (N=176)',
-#               x='', y='Median CAGE TPM') +
-#          scale_color_brewer(palette='Set2', name='', direction=-1)
